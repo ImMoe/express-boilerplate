9d0c8079404bb8f35c5293385e2c3498bb5e7f09
Your Name <you@example.com>
2020-03-16 23:11:09 +0100

    Refactoring controller file

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,3 +1,10 @@
+/**
+ * @index => Start
+ * @show => Show single item
+ * @create => Create new item / items
+ * @update => Update item / items
+ * @destroy => Delete item / items
+ */
 exports.index = (req, res) => {
-    res.render("index");
-}
+  res.render("index");
+};
