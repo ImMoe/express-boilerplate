24b7592752a64447f4cda31dc7608aec138e921a
Your Name <you@example.com>
2020-03-19 21:36:17 +0100

    Updating controllers and adding new packages.

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,10 +1,15 @@
 /**
  * @index => Start
- * @show => Show single item
- * @create => Create new item / items
- * @update => Update item / items
- * @destroy => Delete item / items
+ * @show => Show single
+ * @create => Insert queries
+ * @update => Update query
+ * @destroy => Remove query from database
  */
+
+const Message = require("../models/Message");
+
 exports.index = (req, res) => {
+  // const firstMessage = new Message({ message: "Hello World!" });
+  // firstMessage.save();
   res.render("index");
 };
